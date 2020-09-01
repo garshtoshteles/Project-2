9db0d69f2b8cee05087ede6f1b4a0e9070af8a8d
garshtoshteles <tj.yuxn.scott@gmail.com>
2020-08-31 20:52:18 -0400

    cleaned up indexhtml and added crool.js pseud

---
+++ crool.js
@@ -0,0 +1,15 @@
+// The insults must be retrieved from the database
+
+// The bot asks questions about the user to narrow down their insults.
+
+// // The first question is whteher or not they want any specific insults at all or rather just general insults
+
+// // If user gives a response that isn't yes/no to the questions, hurl an insult at them at that moment. "Did you even think that through?"
+
+// The user's slider selection will narrow down insults by intensity
+
+// Insults must be pushed into chat bubbles belonging to the bot, in ascending recency
+
+// User's input must be pushed up into a bubble at the bottom of the chat
+
+// User must be able to give an insult and have the computer recognize it. Keyword?
