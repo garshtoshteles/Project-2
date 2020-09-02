822d40a4ae0de57f2abb83295553839bc747f4b7
honeybee <63674362+mandylloyd@users.noreply.github.com>
2020-09-02 18:52:07 -0400

    added some js thoughts

--- crool.js
+++ crool.js
@@ -1,3 +1,5 @@
+
+
 // The insults must be retrieved from the database
 
 // The bot asks questions about the user to narrow down their insults.
@@ -13,3 +15,39 @@
 // User's input must be pushed up into a bubble at the bottom of the chat
 
 // User must be able to give an insult and have the computer recognize it. Keyword?
+
+
+// -------------------------------------------------------------------------
+
+// this is just me getting my thoughts together -mandy
+
+// load site and first chat bubble from bot says something to begin conversation
+
+        //document ready function?
+
+// user replies, reply is pushed into a chat bubble and logged
+
+    // function to grab input of textarea & store it for use when user clicks send
+    // function to clear input field
+
+// reply is determined to be yes/no/other
+
+    // function to categorize the input
+
+// while we log/categorize response, have bot thinking
+
+    // set timeout function to add a realistic feature of someone thinking, not too long!
+
+// a random insult is pulled from database from yes/no/other category is pushed to the bot chat bubble
+
+    // function to pull apropriate category insult from database
+    // function should randomize it and then remove it from future insults
+
+    // function to put response into a chat bubble and push it to the bottom of the chat
+
+// that insult should be taken out of future responses to not be repeated
+
+
+// feature to add insults to database
+
+
