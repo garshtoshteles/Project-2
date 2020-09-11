c9010fac5ab1076260f0fb32531e51a9b1b39254
honeybee <63674362+mandylloyd@users.noreply.github.com>
2020-09-11 17:28:09 -0400

    added some more functionality

--- crool.js
+++ crool.js
@@ -1,51 +1,40 @@
-// The insults must be retrieved from the database
-
-// The bot asks questions about the user to narrow down their insults.
-
-// // The first question is whteher or not they want any specific insults at all or rather just general insults
-
-// // If user gives a response that isn't yes/no to the questions, hurl an insult at them at that moment. "Did you even think that through?"
-
-// The user's slider selection will narrow down insults by intensity
-
-// Insults must be pushed into chat bubbles belonging to the bot, in ascending recency
-
-// User's input must be pushed up into a bubble at the bottom of the chat
-
-// User must be able to give an insult and have the computer recognize it. Keyword?
-
-// -------------------------------------------------------------------------
-
-// this is just me getting my thoughts together -mandy
-
-// load site and first chat bubble from bot says something to begin conversation
-
-//document ready function?
-
-// user replies, reply is pushed into a chat bubble and logged
-
-// function to grab input of textarea & store it for use when user clicks send
 let inputVal;
 const inputArr = [];
 
+//load document and bot asks how mean in a 1 second delayed chat bubble
+$(function() {
+  setTimeout(function(){
+  let bubble = $("<p>").addClass("bot-bubble").append("Looking for a rude conversation or just straight up mean?");
+  $("#newBub").append(bubble);
+},1000);
+});
+
+//user replies with preference I DONT KNOW HOW TO FINISH THIS CODE
+function howMean() {
+  inputVal = $("#userInput").val();
+  
+  //if input value equals "rude" then pull from 1 ratings
+  //else pull from 2 ratings
+
+  //???
+
+}
+
+//get user input
 function getInputValue() {
   inputVal = $("#userInput").val();
   inputArr.push(inputVal);
-  console.log("gIV done");
   pushUserBubble();
 }
 
-// function to plug input of textarea into a chat bubble and clear input field
-
+//push user input into chat bubble
 function pushUserBubble() {
-  let bubble = $("<p>").html($("#userInput").val());
+  let bubble = $("<p>").addClass("user-bubble").html($("#userInput").val());
   $("#newBub").append(bubble);
   $("#userInput").val("");
-  console.log("bubble pushed");
 }
 
 // reply is determined to be yes/no/other
-
 function determineIO() {
   let userResponse = inputVal.toLowerCase();
   console.log("determineIO running.");
@@ -59,6 +48,7 @@ function determineIO() {
   }
 }
 
+//possible user inputs for yes
 const yesArr = [
   "yeah",
   "y",
@@ -72,6 +62,8 @@ const yesArr = [
   "i have",
   "of course",
 ];
+
+//possible user inputs for no
 const noArr = [
   "no",
   "nah",
@@ -83,6 +75,7 @@ const noArr = [
   "of course not",
 ];
 
+//determin yes response
 function isAffirmative(text) {
   for (let i = 0; i < yesArr.length; i++) {
     if (text == yesArr[i]) {
@@ -92,6 +85,7 @@ function isAffirmative(text) {
   return false;
 }
 
+//determin no response
 function isNegative(text) {
   for (let i = 0; i < noArr.length; i++) {
     if (text == noArr[i]) {
@@ -101,19 +95,13 @@ function isNegative(text) {
   return false;
 }
 
-// function to categorize the input
-
-// while we log/categorize response, have bot thinking maybe ???
-
-// set timeout function to add a realistic feature of someone thinking, not too long!
-
-// a random insult is pulled from database from yes/no/other category is pushed to the bot chat bubble
-
 // function to pull apropriate category insult from database
 // function should randomize it and then remove it from future insults
 
-// function to put response into a chat bubble and push it to the bottom of the chat
-
-// that insult should be taken out of future responses to not be repeated
+// function to put response into a chat bubble from the bot, needs an event listener
+// setTimeout(function(){
+//   let bubble = $("<p>").addClass("bot-bubble").append(" ");
+//   $("#newBub").append(bubble);
+// },1000);
 
 // feature to add insults to database
