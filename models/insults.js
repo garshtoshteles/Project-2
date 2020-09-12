3f1701110ccba5687f558c7eeaefa9b74f18f70e
garshtoshteles <tj.yuxn.scott@gmail.com>
2020-09-11 20:05:08 -0400

    created skeleton routes, tore out useless requirements in crool.js

--- models/insults.js
+++ models/insults.js
@@ -1,26 +1,27 @@
 // Creating our User model
 var Sequelize = require("sequelize");
-module.exports = function(sequelize, DataTypes) {
-  const Insults = sequelize.define("Insults", {
-    contents: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    intensity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
+var sequelize = require("../CroolDBConnection.js");
 
-    createdAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.fn("NOW"),
-    },
-    updatedAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.fn("NOW"),
-    },
-  });
+const Insults = sequelize.define("Insults", {
+  contents: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  intensity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.fn("NOW"),
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.fn("NOW"),
+  },
+});
 
-  return Insults;
-};
+Insults.sync();
+
+module.exports = Insults;
